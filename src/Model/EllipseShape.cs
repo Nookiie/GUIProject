035ea390e1925c46d284c19e21086a688183a12e
Gorac11 <ivand10@abv.bg>
2019-05-06 16:15:03 +0300

    Update EllipseShape.cs

--- src/Model/EllipseShape.cs
+++ src/Model/EllipseShape.cs
@@ -45,14 +45,18 @@ namespace Draw
             float radiusY = Rectangle.Height / 2;
 
             float centerX = Rectangle.Left + radiusX;
-            float centerY = Rectangle.Bottom + radiusY;
+            float centerY = Rectangle.Top + radiusY;
 
+            double compare=Math.Sqrt(Math.Pow((point.X - centerX), 2)+Math.Pow((point.Y-centerY),2));
+
+             // double ResultFormula = (Math.Pow(radiusY,2) * Math.Pow(point.X,2)) + (Math.Pow(radiusX,2) * Math.Pow(point.Y,2)) - (Math.Pow(radiusX,2) * Math.Pow(radiusY,2));
             /*
-             * double ResultFormula = (Math.Pow(radiusY,2) * Math.Pow(point.X,2)) + (Math.Pow(radiusX,2) * Math.Pow(point.Y,2)) - (Math.Pow(radiusX,2) * Math.Pow(radiusY,2));
-             * 
+            * 
              * geeksforgeeks.org/midpoint-ellipse-drawing-algorithm - Може да ни е от полза
              */
-            if (centerX*radiusY*radiusY + centerY*radiusX*radiusX <= radiusX*radiusX*radiusY*radiusY) // Something similiar needed for the algorithm
+            // if (point.X*point.X*radiusY*radiusY + point.Y*radiusX*radiusX <= radiusX*radiusX*radiusY*radiusY) // Something similiar needed for the algorithm
+            //if (ResultFormula<=0) 
+            if(compare<=radiusX)
                 return true;    
             else
                 return false;  
