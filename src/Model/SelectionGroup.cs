b5ccbd87f9fde20cacb1e51a0404bba3f6d52a48
Viktor Ivanov <33068739+Nookiie@users.noreply.github.com>
2019-05-14 19:18:46 +0300

    Made backup

---
+++ src/Model/SelectionGroup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Draw.src.Model
+{
+    public class SelectionGroup
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int numberOfShapes;
+        public int NumberOfShapes
+        {
+            get { return numberOfShapes; }
+            set { numberOfShapes = value; }
+        }
+
+        private List<Shape> selections;
+        public List<Shape> Selections
+        {
+            get { return selections; }
+            set { selections = value; }
+        }
+
+        public SelectionGroup(string name, List<Shape> selections)
+        {
+            this.Name = name;
+            this.NumberOfShapes = Selections.Count;
+            this.Selections = selections;
+        }
+    }
+}
---
+++ src/Model/Shapes/Shape.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Draw
+{
+    /// <summary>
+    /// Базовия клас на примитивите, който съдържа общите характеристики на примитивите.
+    /// </summary>
+    public abstract class Shape
+    {
+        #region Constructors
+
+        public Shape()
+        {
+
+        }
+
+        public Shape(RectangleF rect)
+        {
+            rectangle = rect;
+        }
+
+        public Shape(PointF pt1, PointF pt2)
+        {
+            point1 = pt1;
+            point2 = pt2;
+        }
+
+        public Shape(Shape shape)
+        {
+            this.Height = shape.Height;
+            this.Width = shape.Width;
+            this.Location = shape.Location;
+            this.rectangle = shape.rectangle;
+            this.Rotation = shape.Rotation;
+
+            this.FillColor = shape.FillColor;
+            this.BorderColor = shape.BorderColor;
+        }
+
+        public Shape(Bitmap image)
+        {
+            this.Height = image.Height;
+            this.Width = image.Width;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Обхващащ правоъгълник на елемента.
+        /// </summary>
+        private RectangleF rectangle;
+        public virtual RectangleF Rectangle
+        {
+            get { return rectangle; }
+            set { rectangle = value; }
+        }
+
+        private PointF point1;
+        public virtual PointF Point1
+        {
+            get { return point1; }
+            set { point1 = value; }
+        }
+
+        private PointF point2;
+        public virtual PointF Point2
+        {
+            get { return point2; }
+            set { point2 = value; }
+        }
+
+        /// <summary>
+        /// Широчина на елемента.
+        /// </summary>
+        public virtual float Width
+        {
+            get { return Rectangle.Width; }
+            set { rectangle.Width = value; }
+        }
+
+        /// <summary>
+        /// Височина на елемента.
+        /// </summary>
+        public virtual float Height
+        {
+            get { return Rectangle.Height; }
+            set { rectangle.Height = value; }
+        }
+
+        public virtual SizeF Size
+        {
+            get { return Rectangle.Size; }
+            set { rectangle.Size = value; }
+        }
+
+        public virtual float Rotation { get; set; } // Needs to be set correctly later on
+
+        /// <summary>
+        /// Горен ляв ъгъл на елемента.
+        /// </summary>
+        public virtual PointF Location
+        {
+            get { return Rectangle.Location; }
+            set { rectangle.Location = value; }
+        }
+
+        /// <summary>
+        /// Цвят на елемента.
+        /// </summary>
+        private Color fillColor;
+        public virtual Color FillColor
+        {
+            get { return fillColor; }
+            set { fillColor = value; }
+        }
+
+        private Color borderColor;
+        public virtual Color BorderColor
+        {
+            get { return borderColor; }
+            set { borderColor = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Проверка дали точка point принадлежи на елемента.
+        /// </summary>
+        /// <param name="point">Точка</param>
+        /// <returns>Връща true, ако точката принадлежи на елемента и
+        /// false, ако не пренадлежи</returns>
+        public virtual bool Contains(PointF point)
+        {
+            return Rectangle.Contains(point.X, point.Y); // Drawable
+        }
+
+        /// <summary>
+        /// Визуализира елемента.
+        /// </summary>
+        /// <param name="grfx">Къде да бъде визуализиран елемента.</param>
+        public virtual void DrawSelf(Graphics grfx)
+        {
+            // shape.Rectangle.Inflate(shape.BorderWidth, shape.BorderWidth);
+        }
+
+        public virtual void DrawSelf(Graphics grfx, PointF pt1, PointF pt2)
+        {
+            grfx.DrawLine(new Pen(BorderColor == Color.Empty ? Color.Black : BorderColor),pt1,pt2);
+        }
+
+        /// <summary>
+        /// Изтрива елемента
+        /// </summary>
+        /// <param name="grfx"></param>
+        public virtual void Remove(Graphics grfx)
+        {
+            grfx.Dispose();
+        }
+
+        public virtual void Rotate(Graphics grfx)
+        {
+            grfx.Transform.Rotate(20); // From the Center
+        }
+
+        public virtual GraphicsPath GetPath(Rectangle bounds)
+        {
+            GraphicsPath path = new GraphicsPath();
+
+            return path;
+        }
+
+        #endregion
+    }
+}
